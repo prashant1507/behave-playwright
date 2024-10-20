39b1e2bd8cce4d8237af8f85fdc53ddba3f8bb64
Prashant Jeet Singh <pjeet.0733@gmail.com>
2024-10-20 18:19:30 +0400

    helper_utils.py

--- utils/helper_utils.py
+++ utils/helper_utils.py
@@ -7,8 +7,12 @@ from helpers.constants.framework_constants import FrameworkConstants as Fc
 
 
 def prepare_dirs():
-    dir_structure = [Fc.reports_parent_dir, Fc.allure_json_dir, Fc.allure_html_dir, Fc.html_dir, Fc.logs_dir, Fc.json_dir,
-                     Fc.pretty_dir, Fc.rerun_dir, Fc.screenshots_dir]
+    dir_structure = [
+        Fc.reports_parent_dir, Fc.allure_json_dir, 
+        Fc.allure_html_dir, Fc.html_dir, 
+        Fc.logs_dir, Fc.json_dir, 
+        Fc.pretty_dir, Fc.rerun_dir, Fc.screenshots_dir
+    ]
 
     details = read_file(Fc.details_file)
     if details["delete_old_reports"]:
@@ -18,10 +22,14 @@ def prepare_dirs():
 
 
 def execute_command_using_run(command):
-    return subprocess.run(str(command).split(" "), capture_output=True, text=True, check=True)
+    return subprocess.run(
+        command.split(), capture_output=True, text=True, check=True
+    )
 
 def execute_command_using_popen(command):
-    return subprocess.Popen(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1)
+    return subprocess.Popen(
+        command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1
+    )
 
 
 def create_dir(folder):
@@ -37,5 +45,4 @@ def delete_dir(folder):
 
 def read_file(path):
     with open(path, "r") as f:
-        data = json.load(f)
-    return data
+        return json.load(f)
