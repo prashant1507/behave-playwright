a5f9577b90978007b1dd707336e978423ef10a93
Prashant Jeet Singh <pjeet.0733@gmail.com>
2024-10-20 18:31:10 +0400

    generate_report.py

--- utils/reporting/generate_report.py
+++ utils/reporting/generate_report.py
@@ -11,10 +11,11 @@ def generate_allure_report(logger):
     current_time = datetime.datetime.now()
     file_name = current_time.strftime("%d_%m_%y-%H_%M_%S_%f")[:-3]
     report = f"{Fc.allure_html_dir}/{file_name}"
-    execute_command_using_run(f"allure generate {Fc.allure_json_dir} --report-dir {report} --clean --report-name Test_Report --single-file")
 
+    command = f"allure generate {Fc.allure_json_dir} --report-dir {report} --clean --report-name Test_Report --single-file"
     # To generate allure report with all files and folders
-    # execute_command_using_run(f"allure generate {Fc.allure_json_dir} --report-dir {report} --clean --report-name Test_Report")
+    # command = f"allure generate {Fc.allure_json_dir} --report-dir {report} --clean --report-name Test_Report"
+    execute_command_using_run(command)
     logger.info(f"HTML output report is located at: {report}/index.html")
     send_report(logger, report)
     return f"{report}/index.html"
@@ -25,6 +26,12 @@ def set_environment_variables():
         "Browser": details["browser"]
     }
 
-    with open(f"{Fc.allure_json_dir}/environment.properties", "w") as file:
-        for key, value in environment_data.items():
-            file.write(f"{key}={value}\n")
+    env_file_path = f"{Fc.allure_json_dir}/environment.properties"
+    
+    try:
+        with open(env_file_path, "w") as file:
+            for key, value in environment_data.items():
+                file.write(f"{key}={value}\n")
+    except IOError as e:
+        logger.info(f"Failed to write environment properties to {environment_file_path}: {e}")
+        pass
